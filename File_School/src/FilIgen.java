b886c3d65c098e24e9045a796e3fb2e73b6d8b1d
Patrick <pkkann@gmail.com>
2013-02-14 13:47:32 +0100

    FILE

---
+++ File_School/src/FilIgen.java
@@ -0,0 +1,47 @@
+
+import java.io.*;
+
+/**
+ *
+ * @author Patrick
+ */
+public class FilIgen {
+    
+    private DataOutputStream dos;
+    private DataInputStream dis;
+
+    public FilIgen(String fileName) {
+        
+        try {
+            dos = new DataOutputStream(new FileOutputStream(fileName));
+            dis = new DataInputStream(new FileInputStream(fileName));
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found");
+        }
+    }
+    
+    public void writeLn(String str) {
+        try {
+            dos.writeBytes(str);
+        } catch (IOException ex) {
+            System.out.println("Could not write");
+        }
+    }
+    
+    public String readFirstLn() {
+        return "";
+    }
+    
+    public String readLn(int line) {
+        return "";
+    }
+    
+    public void close() {
+        try {
+            dos.close();
+            dis.close();
+        } catch (IOException ex) {
+            System.out.println("Could not close");
+        }
+    }
+}
---
+++ File_School/src/Main.java
@@ -0,0 +1,19 @@
+
+/**
+ *
+ * @author Patrick
+ */
+public class Main {
+    
+    private FilIgen fi;
+    
+    public Main() {
+        fi = new FilIgen("C://Users//Patrick//Documents//test.txt");
+    }
+    
+    public static void main(String args[]) {
+        Main main = new Main();
+        main.fi.writeLn("hest");
+        main.fi.close();
+    }
+}
