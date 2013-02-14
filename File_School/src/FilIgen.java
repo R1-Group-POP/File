c41bdb6138a96575b05808b8be32213d98de0e85
Patrick <pkkann@gmail.com>
2013-02-14 22:28:54 +0100

    Removed all imports

--- File_School/src/FilIgen.java
+++ File_School/src/FilIgen.java
@@ -1,17 +1,4 @@
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 
