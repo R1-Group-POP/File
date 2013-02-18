0e608ebe3b516192352e992ce5945ccf9842eb34
Patrick Knappmann <7h380x@live.dk>
2013-02-18 13:39:42 +0100

    REQUIRED MESSAGE OF IMPORTANCE

--- File_School/src/Main.java
+++ File_School/src/Main.java
@@ -1,4 +1,13 @@
 
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 /**
  *
  * @author Patrick
@@ -8,13 +17,18 @@ public class Main {
     private FilIgen fi;
     
     public Main() {
-        fi = new FilIgen("C://Users//Patrick//Desktop//test.txt");
+        fi = new FilIgen("C://Users//Patrick//Documents//test.dat");
     }
     
-    public static void main(String args[]) {
-        Main main = new Main();
-        main.fi.writeLn("Hesteged");
-        System.out.println(main.fi.readFirstLn());
-        main.fi.close();
-    }
+   // public static void main(String args[]) {
+//        try {
+//            DataOutputStream dout = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("C://Users//Patrick//Documents//test.dat")));
+//            dout.write(10);
+//            dout.write(12);
+//            dout.close();
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        }
+//        
+//    }
 }
